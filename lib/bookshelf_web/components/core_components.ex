448a6402a7be03cf621f45923696c881ae0b7acc
Hugo Fabre <hfabre@synbioz.com>
2024-08-12 16:02:13 +0200

    awesome design changes

--- lib/bookshelf_web/components/core_components.ex
+++ lib/bookshelf_web/components/core_components.ex
@@ -729,7 +729,7 @@ defmodule BookshelfWeb.CoreComponents do
 
         <.modal id={"modal-#{book.id}"}>
           <div class="flex flex-row items-start mb-4">
-            <img class="basis-1/4 shrink mr-4" src={"data:#{book.cover_type};base64, #{book.cover}"} alt={"#{book.title} cover"} />
+            <img class="basis-1/4 shrink mr-4 max-w-72" src={"data:#{book.cover_type};base64, #{book.cover}"} alt={"#{book.title} cover"} />
 
             <div class="basis-3/4 flow-root mb-16">
               <dl class="-my-3 divide-y divide-gray-100 text-sm">
@@ -780,7 +780,13 @@ defmodule BookshelfWeb.CoreComponents do
                 <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
               </svg>
             </.link>
-            </div>
+
+            <.link href={~p"/books/#{book.id}"} method="delete">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+              </svg>
+            </.link>
+          </div>
 
           <%= HtmlSanitizeEx.strip_tags(book.description) %>
         </.modal>
