4ea58f966daa8874e3be433c7ffd0fabc2f55277
Hugo Fabre <hugo.fabre@wecasa.fr>
2026-07-03 11:03:45 +0200

    ci cleanup

--- db/migrate/20251121194327_add_user_to_authors.rb
+++ db/migrate/20251121194327_add_user_to_authors.rb
@@ -1,7 +1,7 @@
 class AddUserToAuthors < ActiveRecord::Migration[8.0]
   def change
     add_reference :authors, :user, null: false, foreign_key: true
-    remove_index :authors, [:name]
-    add_index :authors, [:user_id, :name], unique: true
+    remove_index :authors, [ :name ]
+    add_index :authors, [ :user_id, :name ], unique: true
   end
 end
